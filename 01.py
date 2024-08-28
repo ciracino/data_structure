d3c3884cf498eb94f10626286cc18e5b77387518
user <ciracino88@gmail.com>
2024-08-28 09:48:21 +0900

    가방 문제 관련 함수

--- 01.py
+++ 01.py
@@ -1,7 +1,7 @@
 def contains(bag, e):
     return e in bag
 
-def contains(bag, e):
+def insert(bag, e):
     bag.append(e)
 
 def remove(bag, e):
@@ -9,3 +9,18 @@ def remove(bag, e):
 
 def count(bag):
     return len(bag)
+
+my_bag = []
+insert(my_bag, '가방')
+insert(my_bag, '사과')
+insert(my_bag, '배')
+insert(my_bag, '물')
+insert(my_bag, '장난감')
+print(my_bag)
+print(count(my_bag))
+print(contains(my_bag, "물"))
+print(contains(my_bag, "사과"))
+print(contains(my_bag, "꽃"))
+remove(my_bag, '사과')
+print(contains(my_bag, "사과"))
+print(count(my_bag))
